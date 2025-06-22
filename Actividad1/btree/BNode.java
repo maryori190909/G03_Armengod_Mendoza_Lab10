8562c7169648bcaecbe057440425c1004400df60
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-21 22:58:51 -0500

    v4

--- Actividad1/btree/BNode.java
+++ Actividad1/btree/BNode.java
@@ -3,4 +3,8 @@ import exeptions.*;
 
 public class BNode<E extends Comparable<E>>{
 
+    protected ArrayList<E> keys;
+    protected ArrayList <BNode<E>> childs;
+    protected int count;
+    protected int idNode;
 }
