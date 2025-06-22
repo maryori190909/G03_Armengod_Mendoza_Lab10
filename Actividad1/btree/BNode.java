9f387f9b7b6c0da82a3ea8dbc60cdb421e8f7093
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-21 23:08:12 -0500

    v5

--- Actividad1/btree/BNode.java
+++ Actividad1/btree/BNode.java
@@ -7,4 +7,17 @@ public class BNode<E extends Comparable<E>>{
     protected ArrayList <BNode<E>> childs;
     protected int count;
     protected int idNode;
+
+    public BNode(int n) {
+        this.keys = new ArrayList<>(n);
+        this.childs = new ArrayList<>(n + 1);
+        this.count = 0;
+
+        for (int i = 0; i < n - 1; i++){
+            this.keys.add(null);}
+        for (int i = 0; i < n; i++){
+            this.childs.add(null);}
+        }
+    
+
 }
