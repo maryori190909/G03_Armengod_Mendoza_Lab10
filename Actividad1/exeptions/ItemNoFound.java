914537bd7ad1a45d648ab158e10edb5f575e474b
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-22 21:56:20 -0500

    v27

--- Actividad1/exeptions/ItemNoFound.java
+++ Actividad1/exeptions/ItemNoFound.java
@@ -1,5 +1,10 @@
 package Actividad1.exeptions;
 
-public class ItemNoFound {
-
+public class ItemNoFound extends Exception{
+    public ItemNoFound(String mens){
+    super(mens);
+    }
+    public ItemNoFound(){
+    super("El elemento no se encontro en la estructura");
+    }
 }
