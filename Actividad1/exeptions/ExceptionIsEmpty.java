ecd3c5cb74dcb097fe720028f1d5685ea8a3b5d4
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-22 21:51:15 -0500

    v23

--- Actividad1/exeptions/ExceptionIsEmpty.java
+++ Actividad1/exeptions/ExceptionIsEmpty.java
@@ -1,3 +1,11 @@
 public class ExceptionIsEmpty extends Exception{
 
+    public ExceptionIsEmpty(String mens){
+    super(mens);
+    }
+    public ExceptionIsEmpty(){
+    super("La estructura se encuentra vacia");
+    }
+
+
 }
