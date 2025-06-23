f8ac3483dfe3cf9c508c9c59672a5b39382434c9
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-22 22:09:47 -0500

    V30

--- Actividad1/Main.java
+++ Actividad1/Main.java
@@ -8,7 +8,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        BTree<Integer> arbol = new BTree<>(4);
+        int opcion;
+        do {
+            System.out.println("\n--- MENÚ ÁRBOL B ---");
+            System.out.println("1. Insertar clave");
+            System.out.println("2. Buscar clave");
+            System.out.println("3. Mostrar árbol");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
         
     }
 
+}
 }
