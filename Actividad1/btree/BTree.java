505130198c7a588299ddcd39fb0b5d02fa5e1e52
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-22 21:35:51 -0500

    v12

--- Actividad1/btree/BTree.java
+++ Actividad1/btree/BTree.java
@@ -1,3 +1,11 @@
+
+import java.util.ArrayList;
+import exeptions.*;
 public class BTree<E extends Comparable<E>> {
+
+    private BNode<E> root;
+    private int orden;
+    private boolean up;
+    private BNode<E> nDes;
 
 }
