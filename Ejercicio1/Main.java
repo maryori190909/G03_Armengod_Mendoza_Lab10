6fb16cc54139ff8d77531a5a714c0e96c87f1a6c
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-25 12:10:10 -0500

    v14

--- Ejercicio1/Main.java
+++ Ejercicio1/Main.java
@@ -1,5 +1,10 @@
 package Ejercicio1;
+import Ejercicio1.btre.BTree;
+import Ejercicio1.btre.RegistroEstudiante;
 
 public class Main {
+    public static void main(String[] args) {
+        BTree<RegistroEstudiante> arbol = new BTree<>(4);
+    }
 
 }
