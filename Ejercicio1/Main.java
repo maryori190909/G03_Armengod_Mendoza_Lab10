e39abccd612baea555a555ff3c446a10df843fc4
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-06-25 12:11:26 -0500

    v15

--- Ejercicio1/Main.java
+++ Ejercicio1/Main.java
@@ -5,6 +5,21 @@ import Ejercicio1.btre.RegistroEstudiante;
 public class Main {
     public static void main(String[] args) {
         BTree<RegistroEstudiante> arbol = new BTree<>(4);
+
+        arbol.insert(new RegistroEstudiante(103, "Ana"));
+        arbol.insert(new RegistroEstudiante(110, "Luis"));
+        arbol.insert(new RegistroEstudiante(101, "Carlos"));
+        arbol.insert(new RegistroEstudiante(120, "Lucía"));
+        arbol.insert(new RegistroEstudiante(115, "David"));
+        arbol.insert(new RegistroEstudiante(125, "Jorge"));
+        arbol.insert(new RegistroEstudiante(140, "Camila"));
+        arbol.insert(new RegistroEstudiante(108, "Rosa"));
+        arbol.insert(new RegistroEstudiante(132, "Ernesto"));
+        arbol.insert(new RegistroEstudiante(128, "Denis"));
+        arbol.insert(new RegistroEstudiante(145, "Enrique"));
+        arbol.insert(new RegistroEstudiante(122, "Karina"));
+        arbol.insert(new RegistroEstudiante(108, "Juan"));
     }
+
 
 }
